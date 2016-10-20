f7201347fe13f8da062ae020b5c88245ff598acf
wangling <wang91ling@163.com>
2016-10-20 10:50:29 +0800

    添加测试的提示信息

--- app/src/main/java/com/jaydenxiao/androidfire/ui/resource/fragment/Demo03Fragment.java
+++ app/src/main/java/com/jaydenxiao/androidfire/ui/resource/fragment/Demo03Fragment.java
@@ -50,6 +50,7 @@ public class Demo03Fragment extends BaseFragment {
 
     @OnClick(R.id.tv_check_version_update)
     public void checkVersionUpdate(){
+        System.out.println("检测版本更新");
         Toast.makeText(getContext(),"检测版本更新",Toast.LENGTH_SHORT).show();
     }
 
